a416f38fd6710e509243ad6fc17d169db352ac23
VanityNie <1162435706@qq.com>
2023-08-31 23:47:26 +0800

    增加定义宏处理 数据定义 断言处理 符号表处理

---
+++ define.h
@@ -0,0 +1,100 @@
+#pragma once
+
+using u8 = unsigned char;
+using u16 = unsigned short;
+using u32 = unsigned int;
+using u64 = unsigned long long ;
+
+using i8 = signed char;
+using i16 = signed short;
+using i32 = signed int;
+using i64 = signed long long;
+
+using f32 = float;
+using f64 = double;
+
+//Boolean types
+
+using b32 = int;
+using b8 = char;
+
+//support clang and gcc Complier 
+
+#if defined(__clang__) || defined(__gcc__)
+#define STATIC_ASSERT _Static_assert
+#else
+#define STATIC_ASSERT static_assert
+#endif
+
+
+STATIC_ASSERT(sizeof(u8) == 1, "Expected u8 to be 1 byte");
+STATIC_ASSERT(sizeof(u16) == 2, "Expected u16 to be 2 bytes");
+STATIC_ASSERT(sizeof(u32) == 4, "Expected u32 to be 4 bytes.");
+STATIC_ASSERT(sizeof(u64) == 8, "Expected u64 to be 8 bytes.");
+
+STATIC_ASSERT(sizeof(i8) == 1, "Expected i8 to be 1 byte.");
+STATIC_ASSERT(sizeof(i16) == 2, "Expected i16 to be 2 bytes.");
+STATIC_ASSERT(sizeof(i32) == 4, "Expected i32 to be 4 bytes.");
+STATIC_ASSERT(sizeof(i64) == 8, "Expected i64 to be 8 bytes.");
+
+STATIC_ASSERT(sizeof(f32) == 4, "Expected f32 to be 4 bytes.");
+STATIC_ASSERT(sizeof(f64) == 8, "Expected f64 to be 8 bytes.");
+
+#define TRUE 1
+#define FALSE 0
+
+
+// Platform detection
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) 
+#define KPLATFORM_WINDOWS 1
+#ifndef _WIN64
+#error "64-bit is required on Windows!"
+#endif
+#elif defined(__linux__) || defined(__gnu_linux__)
+// Linux OS
+#define KPLATFORM_LINUX 1
+#if defined(__ANDROID__)
+#define KPLATFORM_ANDROID 1
+#endif
+#elif defined(__unix__)
+// Catch anything not caught by the above.
+#define KPLATFORM_UNIX 1
+#elif defined(_POSIX_VERSION)
+// Posix
+#define KPLATFORM_POSIX 1
+#elif __APPLE__
+// Apple platforms
+#define KPLATFORM_APPLE 1
+#include <TargetConditionals.h>
+#if TARGET_IPHONE_SIMULATOR
+// iOS Simulator
+#define KPLATFORM_IOS 1
+#define KPLATFORM_IOS_SIMULATOR 1
+#elif TARGET_OS_IPHONE
+#define KPLATFORM_IOS 1
+// iOS device
+#elif TARGET_OS_MAC
+// Other kinds of Mac OS
+#else
+#error "Unknown Apple platform"
+#endif
+#else
+#error "Unknown platform!"
+#endif
+
+// import / export the symbol of lib
+#ifdef KEXPORT
+// Exports
+#ifdef _MSC_VER
+#define KAPI __declspec(dllexport)
+#else
+#define KAPI __attribute__((visibility("default")))
+#endif
+#else
+// Imports
+#ifdef _MSC_VER
+#define KAPI __declspec(dllimport)
+#else
+#define KAPI
+#endif
+#endif
