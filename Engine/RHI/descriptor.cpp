8693461d86e5c2f060a785e89d3d7879e1a4962b
VanityNie <1162435706@qq.com>
2024-03-14 00:06:37 +0800

    集成spirv-cross 作为object对象导入cmake

---
+++ Engine/RHI/descriptor.cpp
@@ -0,0 +1,11 @@
+//
+// Created by win111 on 2024/3/6.
+//
+
+#include "descriptor.hpp"
+
+void test()
+{
+    std::vector<uint32_t> spirv_binary = {};
+    spirv_cross::CompilerGLSL glsl(std::move(spirv_binary));
+}
---
+++ Engine/RHI/descriptor.hpp
@@ -0,0 +1,12 @@
+//
+// Created by win111 on 2024/3/6.
+//
+
+#ifndef VULKANENGINE_DESCRIPTOR_H
+#define VULKANENGINE_DESCRIPTOR_H
+#include "spirv_cross/spirv_glsl.hpp"
+
+
+
+void test();
+#endif //VULKANENGINE_DESCRIPTOR_H
